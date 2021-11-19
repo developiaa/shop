f7bd5506934201dd9a9a03b54884838ad559bc80
developiaa <developiaa@gmail.com>
2021-11-19 23:34:17 +0900

    feat : Add data binding when Error interface is used

---
+++ src/main/java/pro/developia/shop/databind/ErrorsSerializer.java
@@ -0,0 +1,55 @@
+package pro.developia.shop.databind;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartArray();
+
+        //필드 에러일 경우
+        errors.getFieldErrors().forEach(e -> {
+            try {
+                gen.writeStartObject();
+
+                gen.writeStringField("field", e.getField());
+//                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if (rejectedValue != null) {
+                    gen.writeStringField("rejectedValue", rejectedValue.toString());
+                }
+                gen.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        });
+
+        //글로벌 에러
+        errors.getGlobalErrors().forEach(e -> {
+            try {
+                gen.writeStartObject();
+
+//                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+
+                gen.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        gen.writeEndArray();
+
+    }
+}
